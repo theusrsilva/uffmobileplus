916e0cba0eb998958570bba20d88496b3b2bd134
JpFontinele-UFF <jfontinele@id.uff.br>
2025-09-10 16:53:37 -0300

    feat: primeira versão da tela principal esqueleto

--- lib/app/modules/internal_modules/dashboard/ui/home_page.dart
+++ lib/app/modules/internal_modules/dashboard/ui/home_page.dart
@@ -1,17 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 import 'package:uffmobileplus/app/modules/internal_modules/dashboard/controller/home_page_controller.dart';
+import 'package:uffmobileplus/app/utils/color_pallete.dart';
 
 class HomePage extends GetView<HomePageController> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-    appBar: AppBar(title: Text('HomePage')),
+      appBar: AppBar(
+        backgroundColor: AppColors.darkBlue(),
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.white,),
+          onPressed: () {
+            // Menu hambúrguer - por enquanto sem ação
+          },
+        ),
+        title: Center(child: Text('UM +', style: TextStyle(color: Colors.white))),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.help_outline, color: Colors.white,),
+            onPressed: () {
+              // Interrogação - por enquanto sem ação
+            },
+          ),
+        ],
+      ),
+      body: PersistentTabView(
+        context,
+        controller: controller.tabController,
+        screens: [
+          Container(child: Center(child: Text('Atalhos'))),
+          Container(child: Center(child: Text('Serviços'))),
+          Container(child: Center(child: Text('Configurações'))),
+        ],
+        items: [
+          // Tela Principal
+          PersistentBottomNavBarItem(
+            icon: Icon(Icons.phonelink_setup),
+            title: "Atalhos",
+            activeColorPrimary: AppColors.lightBlue(),
+            inactiveColorPrimary: Colors.grey,
+            activeColorSecondary: Colors.black,
+          ),
 
-    body: SafeArea(
-      child: Text('HomePageController'))
+          // Tela de Serviços
+          PersistentBottomNavBarItem(
+            icon: Icon(Icons.business),
+            title: "Serviços",
+            activeColorPrimary: AppColors.lightBlue(),
+            inactiveColorPrimary: Colors.black,
+            activeColorSecondary: Colors.black,
+          ),
+
+          // Tela de Configurações
+          PersistentBottomNavBarItem(
+            icon: Icon(Icons.settings),
+            title: "Configurações",
+            activeColorPrimary: AppColors.lightBlue(),
+            inactiveColorPrimary: Colors.grey,
+            activeColorSecondary: Colors.black,
+          ),
+        ],
+        navBarStyle: NavBarStyle.style7,
+      ),
     );
   }
 }
