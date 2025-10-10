868abd4c51dc70249b68666402799449190a7f46
JpFontinele-UFF <jfontinele@id.uff.br>
2025-10-10 13:08:43 -0300

    beta: 6.1.0

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,7 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  rive_native
+  rive_common
   url_launcher_linux
 )
 
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   cloud_firestore
   firebase_auth
   firebase_core
-  rive_native
+  rive_common
   url_launcher_windows
 )
 
